59f213bd17cda3c74d324afb84984c07bc34e8c2
funzter <cavehive@protonmail.com>
2022-04-14 17:14:40 +0200

    fixed inventory see

--- src/main/java/de/timesnake/extension/bukkit/cmd/CmdInventory.java
+++ src/main/java/de/timesnake/extension/bukkit/cmd/CmdInventory.java
@@ -5,19 +5,40 @@ import de.timesnake.basic.bukkit.util.chat.Argument;
 import de.timesnake.basic.bukkit.util.chat.ChatColor;
 import de.timesnake.basic.bukkit.util.chat.CommandListener;
 import de.timesnake.basic.bukkit.util.chat.Sender;
+import de.timesnake.basic.bukkit.util.user.ExItemStack;
 import de.timesnake.basic.bukkit.util.user.User;
+import de.timesnake.basic.bukkit.util.user.event.ExcludedInventoryHolder;
 import de.timesnake.extension.bukkit.chat.Plugin;
 import de.timesnake.library.extension.util.cmd.Arguments;
 import de.timesnake.library.extension.util.cmd.ExCommand;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 public class CmdInventory implements CommandListener, Listener {
+
+    private static final Map<Integer, Integer> PLAYER_SEE_INV = new HashMap<>();
+
+    private static final ExItemStack COMMIT = new ExItemStack(Material.RED_DYE, "§cCommit").setSlot(8);
+
+    static {
+        PLAYER_SEE_INV.put(36, 0); // feet
+        PLAYER_SEE_INV.put(37, 1); // legs
+        PLAYER_SEE_INV.put(38, 2); // chest
+        PLAYER_SEE_INV.put(39, 3); // head
+        PLAYER_SEE_INV.put(40, 5); // off-hand
+    }
 
     @Override
     public void onCommand(Sender sender, ExCommand<Sender, Argument> cmd, Arguments<Argument> args) {
@@ -65,8 +86,80 @@ public class CmdInventory implements CommandListener, Listener {
 
         Player p = sender.getPlayer();
 
-        Inventory inv = arg.toPlayer().getInventory();
+        User seeUser = arg.toUser();
+
+        ItemStack[] items = seeUser.getInventory().getContents();
+
+        VirtuellInventoryHolder holder = new VirtuellInventoryHolder(seeUser);
+        Inventory inv = Server.createExInventory(6 * 9, seeUser.getName(), holder).getInventory();
+        holder.setInventory(inv);
+
+        for (int slot = 0; slot < 41; slot++) {
+            ItemStack item = items[slot];
+
+            if (PLAYER_SEE_INV.containsKey(slot)) {
+                inv.setItem(PLAYER_SEE_INV.get(slot), item);
+            } else {
+                inv.setItem(slot + 18, item);
+            }
+        }
+
+        inv.setItem(COMMIT.getSlot(), COMMIT);
+
         p.openInventory(inv);
+    }
+
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent e) {
+        InventoryHolder holder = e.getView().getTopInventory().getHolder();
+
+        if (!(holder instanceof VirtuellInventoryHolder vHolder)) {
+            return;
+        }
+
+        User seeUser = vHolder.getUser();
+
+        Sender sender = Server.getUser((Player) e.getWhoClicked()).asSender(Plugin.BUKKIT);
+        if (!sender.hasPermission("exbukkit.inventory.modify", 929)) {
+            e.setCancelled(true);
+        }
+
+        ItemStack clickedItem = e.getCurrentItem();
+
+        if (COMMIT.equals(ExItemStack.getItem(clickedItem, false))) {
+            e.setCancelled(true);
+
+            ItemStack[] items = e.getView().getTopInventory().getContents();
+
+            Inventory userInv = seeUser.getInventory();
+
+            userInv.clear();
+
+            for (Map.Entry<Integer, Integer> entry : PLAYER_SEE_INV.entrySet()) {
+                ItemStack item = items[entry.getValue()];
+
+                if (item == null) {
+                    continue;
+                }
+
+                userInv.setItem(entry.getKey(), item);
+            }
+
+            for (int slot = 18; slot < 41; slot++) {
+                ItemStack item = items[slot];
+
+                if (item == null) {
+                    continue;
+                }
+
+                userInv.setItem(slot - 18, item);
+            }
+
+            seeUser.updateInventory();
+
+            return;
+        }
+
     }
 
     public void clear(Sender sender, Argument arg) {
@@ -83,19 +176,40 @@ public class CmdInventory implements CommandListener, Listener {
         sender.sendPluginMessage(ChatColor.PERSONAL + "Cleared inventory from " + ChatColor.VALUE + user.getChatName());
     }
 
-    @EventHandler
-    public void onInventoryClick(InventoryClickEvent e) {
-        org.bukkit.inventory.Inventory inv = e.getInventory();
-        if (e.getView().getTitle().equals("Inventory")) {
-            if (inv.getHolder() != null) {
-                if (!inv.getHolder().equals(e.getWhoClicked())) {
-                    Sender sender = Server.getUser((Player) e.getWhoClicked()).asSender(Plugin.BUKKIT);
-                    if (!sender.hasPermission("exbukkit.inventory.modify", 929)) {
-                        e.setCancelled(true);
-                    }
-                    e.setCancelled(false);
-                }
-            }
+    private static class VirtuellInventoryHolder implements ExcludedInventoryHolder {
+
+        private final User user;
+        private Inventory inventory;
+
+        public VirtuellInventoryHolder(User user) {
+            this.user = user;
+        }
+
+        @NotNull
+        @Override
+        public Inventory getInventory() {
+            return inventory;
+        }
+
+        public void setInventory(Inventory inventory) {
+            this.inventory = inventory;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            VirtuellInventoryHolder that = (VirtuellInventoryHolder) o;
+            return Objects.equals(inventory, that.inventory);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(inventory);
+        }
+
+        public User getUser() {
+            return user;
         }
     }
 
