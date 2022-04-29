287db0cc161a967c3e4e3eba5233a43127a9f48d
funzter <cavehive@protonmail.com>
2022-04-29 21:44:14 +0200

    cleanup

--- src/main/java/de/timesnake/extension/bukkit/cmd/CmdWorld.java
+++ src/main/java/de/timesnake/extension/bukkit/cmd/CmdWorld.java
@@ -48,25 +48,21 @@ public class CmdWorld implements CommandListener {
         ExWorld world = Server.getWorld(worldName);
 
         switch (args.getString(0)) {
-            case "create":
+            case "create" -> {
                 if (!sender.hasPermission("exbukkit.world.create", 949)) {
                     return;
                 }
-
                 if (world != null) {
                     sender.sendMessageWorldAlreadyExist(worldName);
                     return;
                 }
-
                 for (String s : WorldManager.UNSUPPORTED_SYMBOLS) {
                     if (worldName.contains(s)) {
                         sender.sendPluginMessage(ChatColor.WARNING + "World name contains an unsupported symbol: " + ChatColor.VALUE + s);
                         return;
                     }
                 }
-
                 WorldManager.Type worldType = WorldManager.Type.VOID;
-
                 if (args.isLengthEquals(3, false)) {
                     String typeName = args.getString(2).toUpperCase();
                     try {
@@ -79,63 +75,66 @@ public class CmdWorld implements CommandListener {
                     sender.sendMessageTooManyArguments();
                     return;
                 }
-
                 sender.sendPluginMessage(ChatColor.PERSONAL + "Creating world " + ChatColor.VALUE + worldName + ChatColor.PERSONAL + " with type " + ChatColor.VALUE + worldType.name());
                 Server.getWorldManager().createWorld(worldName, worldType);
                 sender.sendPluginMessage(ChatColor.PERSONAL + "Complete");
-                break;
-            case "clone":
+            }
+            case "clone" -> {
                 if (!sender.hasPermission("exbukkit.world.clone", 1)) {
                     return;
                 }
-
                 if (world == null) {
                     sender.sendMessageWorldNotExist(worldName);
                     return;
                 }
-
                 if (!args.isLengthEquals(3, true)) {
                     return;
                 }
-
                 Argument clonedName = args.get(2);
                 if (clonedName.isWorldName(false)) {
                     sender.sendMessageWorldAlreadyExist(worldName);
                     return;
                 }
-
                 sender.sendPluginMessage(ChatColor.PERSONAL + "Cloning world " + ChatColor.VALUE + worldName + ChatColor.PERSONAL + " to " + ChatColor.VALUE + clonedName.getString());
                 Server.getWorldManager().cloneWorld(clonedName.getString(), world);
                 sender.sendPluginMessage(ChatColor.PERSONAL + "Complete");
-
-                break;
-            case "delete":
+            }
+            case "delete" -> {
                 if (!sender.hasPermission("exbukkit.world.delete", 951)) {
                     return;
                 }
-
                 if (world == null) {
                     sender.sendMessageWorldNotExist(worldName);
                     return;
                 }
-
                 if (Server.getWorldManager().deleteWorld(world, true)) {
                     sender.sendPluginMessage(ChatColor.PERSONAL + "Deleted world " + ChatColor.VALUE + worldName);
                 } else {
                     sender.sendPluginMessage(ChatColor.WARNING + "Failed to delete world " + ChatColor.VALUE + worldName);
                 }
-                break;
-            case "teleport":
-            case "tp":
-                if (!sender.hasPermission("exbukkit.world.teleport", 952)) {
+            }
+            case "unload" -> {
+                if (!sender.hasPermission("exbukkit.world.unload", 955)) {
                     return;
                 }
-
                 if (world == null) {
                     sender.sendMessageWorldNotExist(worldName);
                     return;
                 }
-
+                if (Server.getWorldManager().unloadWorld(world, true)) {
+                    sender.sendPluginMessage(ChatColor.PERSONAL + "Unloaded world " + ChatColor.VALUE + worldName);
+                } else {
+                    sender.sendPluginMessage(ChatColor.WARNING + "Failed to unload world " + ChatColor.VALUE + worldName);
+                }
+            }
+            case "teleport", "tp" -> {
+                if (!sender.hasPermission("exbukkit.world.teleport", 952)) {
+                    return;
+                }
+                if (world == null) {
+                    sender.sendMessageWorldNotExist(worldName);
+                    return;
+                }
                 if (args.isLengthEquals(2, false)) {
                     if (!sender.isPlayer(true)) {
                         return;
@@ -148,14 +147,12 @@ public class CmdWorld implements CommandListener {
                     }
                     User user = args.get(2).toUser();
                     user.teleport(world);
-                    user.sendPluginMessage(Plugin.BUKKIT, ChatColor.PERSONAL + "Teleported to world " + ChatColor.VALUE + worldName);
+                    user.sendPluginMessage(Plugin.BUKKIT,
+                            ChatColor.PERSONAL + "Teleported to world " + ChatColor.VALUE + worldName);
                     sender.sendPluginMessage(ChatColor.PERSONAL + "Teleported " + ChatColor.VALUE + user.getChatName() + ChatColor.PERSONAL + " to world " + ChatColor.VALUE + worldName);
                 }
-
-                break;
-            default:
-                this.sendCmdMessages(sender);
-
+            }
+            default -> this.sendCmdMessages(sender);
         }
     }
 
@@ -165,6 +162,9 @@ public class CmdWorld implements CommandListener {
         }
         if (sender.hasPermission("exbukkit.world.delete")) {
             sender.sendMessageCommandHelp("Delete an existing world", "mw delete <world>");
+        }
+        if (sender.hasPermission("exbukkit.world.unload")) {
+            sender.sendMessageCommandHelp("Unload an existing world", "mw unload <world>");
         }
         if (sender.hasPermission("exbukkit.world.teleport")) {
             sender.sendMessageCommandHelp("Teleport to a world", "mw tp <world> [player]");
@@ -181,7 +181,7 @@ public class CmdWorld implements CommandListener {
     public List<String> getTabCompletion(ExCommand<Sender, Argument> cmd, Arguments<Argument> args) {
         if (args.getLength() >= 1) {
             if (args.getLength() == 1) {
-                return List.of("create", "tp", "delete", "clone", "list");
+                return List.of("create", "tp", "delete", "clone", "list", "unload");
             }
             switch (args.getString(0).toLowerCase()) {
                 case "create":
@@ -190,19 +190,16 @@ public class CmdWorld implements CommandListener {
                     }
                     break;
                 case "tp":
-                    if (args.getLength() == 2) {
-                        return Server.getCommandManager().getTabCompleter().getWorldNames();
-                    }
                     if (args.getLength() == 3) {
                         return Server.getCommandManager().getTabCompleter().getPlayerNames();
                     }
                 case "delete":
+                case "unload":
                 case "clone":
                     if (args.getLength() == 2) {
                         return Server.getCommandManager().getTabCompleter().getWorldNames();
                     }
                     break;
-
             }
         }
         return List.of();
