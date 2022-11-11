2c273d8127976e63a7dd5f14e65f27a2f228ffdc
funzter <cavehive@protonmail.com>
2022-11-11 20:31:15 +0100

    add logger to plugins, add log command

--- src/main/java/de/timesnake/extension/bukkit/chat/Plugin.java
+++ src/main/java/de/timesnake/extension/bukkit/chat/Plugin.java
@@ -1,5 +1,5 @@
 /*
- * extension-bukkit.main
+ * timesnake.extension-bukkit.main
  * Copyright (C) 2022 timesnake
  *
  * This program is free software; you can redistribute it and/or
@@ -18,11 +18,16 @@
 
 package de.timesnake.extension.bukkit.chat;
 
+import de.timesnake.library.basic.util.LogHelper;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Plugin extends de.timesnake.basic.bukkit.util.chat.Plugin {
 
-    public static final Plugin BUKKIT = new Plugin("Bukkit", "EXB");
+    public static final Plugin BUKKIT = new Plugin("Bukkit", "EXB", LogHelper.getLogger("Bukkit", Level.INFO));
 
-    protected Plugin(String name, String code) {
-        super(name, code);
+    protected Plugin(String name, String code, Logger logger) {
+        super(name, code, logger);
     }
 }
