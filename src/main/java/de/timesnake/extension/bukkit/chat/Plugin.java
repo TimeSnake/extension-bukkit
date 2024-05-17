2e06f25edfc958d06038b95fe74abf8afd04a176
funzter <funzter@mail.timesnake.de>
2024-05-17 21:41:53 +0200

    minor

--- src/main/java/de/timesnake/extension/bukkit/chat/Plugin.java
+++ src/main/java/de/timesnake/extension/bukkit/chat/Plugin.java
@@ -4,17 +4,11 @@
 
 package de.timesnake.extension.bukkit.chat;
 
-import de.timesnake.library.basic.util.LogHelper;
-
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 public class Plugin extends de.timesnake.basic.bukkit.util.chat.Plugin {
 
-  public static final Plugin BUKKIT = new Plugin("Bukkit", "EXB",
-      LogHelper.getLogger("Bukkit", Level.INFO));
+  public static final Plugin BUKKIT = new Plugin("Bukkit", "EXB");
 
-  protected Plugin(String name, String code, Logger logger) {
+  protected Plugin(String name, String code) {
     super(name, code);
   }
 }
